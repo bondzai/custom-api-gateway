52e43058a056267269be8d34e710966af75011e7
JamesBond <introbond.dev@gmail.com>
2023-08-14 13:56:29 +0700

    Feature: Get wakatime stats

--- wakatime/index.js
+++ wakatime/index.js
@@ -1,23 +1,55 @@
-require('dotenv').config()
+require('dotenv').config();
+const express = require('express');
 const axios = require('axios');
-
+const app = express();
+const port = process.env.PORT || 3358;
 const apiKey = process.env.WAKATIME_APIKEY;
-// const apiUrl = 'https://wakatime.com/api/v1/users/current/stats/last_7_days';
-// const apiUrl = 'https://wakatime.com/api/v1/users/current/all_time_since_today';
-// const apiUrl = 'https://wakatime.com/api/v1/leaders';
+
 const apiUrl = 'https://wakatime.com/api/v1/users/current/stats/all_time';
 
-const getData = (async() =>{
-    return axios.get(apiUrl,{
-        headers: {
-            'Authorization': `Basic ${Buffer.from(apiKey).toString('base64')}`
-        }
-    })
+const fetchWakatimeData = async () => {
+    try {
+        const response = await axios.get(apiUrl, {
+            headers: {
+                'Authorization': `Basic ${Buffer.from(apiKey).toString('base64')}`
+            }
+        });
+
+        const { 
+            human_readable_range,
+            days_including_holidays, 
+            human_readable_total_including_other_language,
+            operating_systems,
+            editors, 
+            languages,
+            best_day,
+        } = response.data.data;
+
+        return {
+            human_readable_range,
+            days_including_holidays,
+            human_readable_total_including_other_language,
+            operating_systems,
+            editors, 
+            languages,
+            best_day,
+        };
+
+    } catch (error) {
+        console.error('Error fetching Wakatime data:', error);
+        throw new Error('An error occurred while fetching data.');
+    }
+};
+
+app.get('/wakatime', async (req, res) => {
+    try {
+        const wakatimeData = await fetchWakatimeData();
+        res.json(wakatimeData);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
 });
 
-const main = (async() => {
-    let data = await getData()
-    console.log(data.data)
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
-
-main()
