af8a1c94a67c7760c8042d631f62e06149b1e0a5
JamesBond <introbond.dev@gmail.com>
2023-08-14 14:31:57 +0700

    Feature: integrated Redis caching

--- wakatime/index.js
+++ wakatime/index.js
@@ -1,40 +1,41 @@
 require('dotenv').config();
 const express = require('express');
 const axios = require('axios');
+const Redis = require('ioredis'); // Import the Redis library
 const app = express();
 const port = process.env.PORT || 3358;
 const apiKey = process.env.WAKATIME_APIKEY;
+const apiUrl = process.env.WAKATIME_URL;
 
-const apiUrl = 'https://wakatime.com/api/v1/users/current/stats/all_time';
+// Create a Redis client
+const redisClient = new Redis(process.env.REDIS_URL); // Use the Redis URL
 
 const fetchWakatimeData = async () => {
     try {
-        const response = await axios.get(apiUrl, {
-            headers: {
-                'Authorization': `Basic ${Buffer.from(apiKey).toString('base64')}`
-            }
-        });
+        const cachedData = await redisClient.get('wakatimeData'); // Check if data exists in Redis cache
+        if (cachedData) {
+            return JSON.parse(cachedData);
+        } else {
+            const response = await axios.get(apiUrl, {
+                headers: {
+                    'Authorization': `Basic ${Buffer.from(apiKey).toString('base64')}`
+                }
+            });
 
-        const { 
-            human_readable_range,
-            days_including_holidays, 
-            human_readable_total_including_other_language,
-            operating_systems,
-            editors, 
-            languages,
-            best_day,
-        } = response.data.data;
+            const wakatimeData = {
+                human_readable_range: response.data.data.human_readable_range,
+                days_including_holidays: response.data.data.days_including_holidays,
+                human_readable_total_including_other_language: response.data.data.human_readable_total_including_other_language,
+                operating_systems: response.data.data.operating_systems,
+                editors: response.data.data.editors,
+                languages: response.data.data.languages,
+                best_day: response.data.data.best_day
+            };
 
-        return {
-            human_readable_range,
-            days_including_holidays,
-            human_readable_total_including_other_language,
-            operating_systems,
-            editors, 
-            languages,
-            best_day,
-        };
-
+            // Store fetched data in Redis cache
+            await redisClient.set('wakatimeData', JSON.stringify(wakatimeData));
+            return wakatimeData;
+        }
     } catch (error) {
         console.error('Error fetching Wakatime data:', error);
         throw new Error('An error occurred while fetching data.');
